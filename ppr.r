6fb007a1a79178bb3b942758c8f92125589296b4
Ivan <privalki@tcd.ie>
2018-07-06 16:45:31 +0100

    Update ppr.r

--- ppr.r
+++ ppr.r
@@ -9,6 +9,8 @@ library(lubridate)
 library(tibbletime)
 library(forecast)
 library(tseries)
+library(ggplot2)
+library(scales)
 
 
 
@@ -78,31 +80,83 @@ qq <- series %>%
 time_ppp <- as_tbl_time(ppp, date)
 time_ppp
 ppp
+q
 
 q <- time_ppp %>%
   select(date, price, descript, notfull) %>%
+  filter(notfull=="No") %>%
   collapse_by("yearly") %>%
-  group_by(date, descript, notfull) %>%
+  group_by(date, descript) %>%
   summarise(n = n(),
-            meanty = mean(price))
+            meanty = mean(price)) 
 
+q$meanty<- parse_number(q$meanty)
 q
 
+
+
+# plot --------------------------------------------------------------------
+my_title <- "Average House Prices Have Risen Year-On-Year"
+my_subtitle <- "Average sales price in the PPR data (Co. Dublin units sold at full price) \nshows a steady rise in prices year on year."
+my_caption <- "Source: Property Price Register @privlko"
 
 ggplot(q) +
   geom_bar(stat = "identity",
            position="dodge",
            aes(x=as.factor(date), y=meanty, fill= descript)) +
   coord_flip() +
-  facet_wrap(~ notfull)
+  scale_x_discrete("Year", 
+                   labels=c("2010", "2011", "2012", "2013", "2014", 
+                            "2015", "2016", "2017", "2018")) +
+  scale_y_continuous("Average Cost",
+                     breaks = seq(100000, 700000, by=200000),
+                     labels = dollar_format(suffix = "", prefix = "€")) +
+  scale_colour_discrete() +
+  labs(title= my_title,
+       subtitle= my_subtitle,
+       caption= my_caption,
+       fill= "Sale Type") +
+  theme_minimal() +
+  scale_fill_discrete(breaks=c("New Dwelling house /Apartment", 
+                               "Second-Hand Dwelling house /Apartment", 
+                               "NA"),
+                      labels=c("New", "Second-Hand", "NA"))
 
+ggsave("ppr_bar.jpg")
+
+??dolar_format
+View(q)
+
+
+
+
+# plot2 -------------------------------------------------------------------
+
+my_title1 <- "Volume of sales has remained steady since 2014"
+my_subtitle <- "Most of the growth in volume of sales (Co.Dublin units at full price) \nhas happened among new builds. There seems to be a ceiling in raw numbers of sales."
+my_caption <- "Source: Property Price Register @privlko"
 
 ggplot(q) +
   geom_bar(stat= "identity",
            position = "dodge",
            aes(x=as.factor(date), y=n, fill=descript))+
   coord_flip() +
-  facet_wrap(~ notfull)
+  scale_x_discrete("Year", 
+                   labels=c("2010", "2011", "2012", "2013", "2014", 
+                            "2015", "2016", "2017", "2018")) +
+  scale_y_continuous("Number of Sales",
+                     breaks = seq(0, 30000, by=5000)) +
+  scale_colour_discrete() +
+  labs(title= my_title,
+       subtitle= my_subtitle,
+       caption= my_caption,
+       fill= "Sale Type") +
+  theme_minimal() +
+  scale_fill_discrete(breaks=c("New Dwelling house /Apartment", 
+                               "Second-Hand Dwelling house /Apartment", 
+                               "NA"),
+                      labels=c("New", "Second-Hand", "NA"))
+
 
 
 View(q)
